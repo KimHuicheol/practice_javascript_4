5878b8cd3a5bcf4ba82bb082e7bc4d47d35e5c8b
KimHuicheol <64347818+KimHuicheol@users.noreply.github.com>
2021-07-10 16:31:55 +0900

    課題4_レビュー追加指摘対応済み_20210710

--- index.js
+++ index.js
@@ -6,15 +6,22 @@ let questionNumber = 1;                                                         
 const quizList = [];
 
 class Quiz {                                                                                            //Quizクラス定義
-  constructor(category, difficulty, question, answerA, answerB, answerC, answerD) {                      //コンストラクター定義
+  constructor(category, difficulty, question, correctAnswer, incorrectAnswerA, incorrectAnswerB, incorrectAnswerC) { 
     this.category = category;                                                                            //カテゴリプロパティ
     this.difficulty = difficulty;                                                                        //難易度プロパティ
     this.question = question;                                                                            //問題文プロパティ
-    this.answerA = answerA;                                                                              //解答選択肢-1プロパティ
-    this.answerB = answerB;                                                                              //解答選択肢-2プロパティ
-    this.answerC = answerC;                                                                              //解答選択肢-3プロパティ
-    this.answerD = answerD;                                                                              //解答選択肢-4プロパティ
+    this.correctAnswer = correctAnswer;                                                                   //正解プロパティ
+    this.incorrectAnswerA = incorrectAnswerA;                                                            //不正解-1プロパティ
+    this.incorrectAnswerB = incorrectAnswerB;                                                            //不正解-2プロパティ
+    this.incorrectAnswerC = incorrectAnswerC;                                                            //不正解-3プロパティ
   } 
+  shuffle(arr) {                                                                                         //配列の値をシャッフルするメソッド
+    for (let j = arr.length - 1; j > 0; j--) {
+      const k = Math.floor(Math.random() * (j + 1));
+      [arr[k], arr[j]] = [arr[j], arr[k]];
+    }
+    return arr;
+  }
   outDisplay() {                                                                                         //クイズ画面出力メソッド定義
     const categoryNode = document.createElement('div');                                                   //カテゴリ出力用の要素作成
     const difficultyNode = document.createElement('div');                                                 //難易度出力用の要素作成
@@ -45,14 +52,18 @@ class Quiz {                                                                    
     answerBtnB.style.display = 'block';
     answerBtnC.style.display = 'block';
     answerBtnD.style.display = 'block';
-    
+
     categoryNode.textContent = '【ジャンル】' + this.category;                                            //カテゴリ出力用要素の出力内容代入
     difficultyNode.textContent = '【難易度】' + this.difficulty;                                          //難易度出力用要素の出力内容代入
     operation.textContent = this.question;                                                                //問題文出力内容の代入
-    answerBtnA.value = this.answerA;                                                                      //解答選択肢-1出力用要素の値代入
-    answerBtnB.value = this.answerB;                                                                      //解答選択肢-2出力用要素の値代入
-    answerBtnC.value = this.answerC;                                                                      //解答選択肢-3出力用要素の値代入
-    answerBtnD.value = this.answerD;                                                                      //解答選択肢-4出力用要素の値代入
+
+    const answers = [this.correctAnswer, this.incorrectAnswerA, this.incorrectAnswerB, this.incorrectAnswerC];
+    const shuffleAnswers = this.shuffle([...answers]);
+
+    answerBtnA.value = shuffleAnswers[0];                                                                      //解答選択肢-1出力用要素の値代入
+    answerBtnB.value = shuffleAnswers[1];                                                                      //解答選択肢-2出力用要素の値代入
+    answerBtnC.value = shuffleAnswers[2];                                                                      //解答選択肢-3出力用要素の値代入
+    answerBtnD.value = shuffleAnswers[3];                                                                      //解答選択肢-4出力用要素の値代入
 
     title.after(categoryNode);                                                                            //各要素の出力
     category.after(difficultyNode);
@@ -61,9 +72,14 @@ class Quiz {                                                                    
     document.body.appendChild(answerBtnC);
     document.body.appendChild(answerBtnD);
 
-    btnA.addEventListener('click', () => {                                                                //正解解答選択肢押下時処理
-      correctAnswerCount++;                                                                                //正解回答数インクリメント
-    });
+    const selectedAnswers = document.getElementsByTagName('input')                                   //動的生成されたinputノードの取得
+    for (let selectedAnswer of selectedAnswers) {                                                    //解答選択肢1~4押下時処理
+      selectedAnswer.addEventListener('click', () => {
+        if(selectedAnswer.value === answers[0]){
+          correctAnswerCount++;                                                                             //正解回答数インクリメント
+        } 
+      });
+    }
   }
 }
 
@@ -87,10 +103,11 @@ window.addEventListener('load', () => {                                         
   async function sendApiRequest() {                                                                             //APIリクエスト送信関数実行
     try {
       const response = await fetch('https://opentdb.com/api.php?amount=10&type=multiple')                       //リクエスト送信
-      if(!response.ok) {
+      if (!response.ok) {
         new Error('エラーレスポンス');
       }
-      const json = await response.json()                                                                        //JSON抽出-クイズ一覧取得
+      const json = await response.json()                                                                        //JSON抽出-クイズ一覧取
+      console.log(json.results);
       json.results.forEach((quizData) => {
         quizList.push(
           new Quiz(
@@ -109,7 +126,7 @@ window.addEventListener('load', () => {                                         
       selectAnswer();                                                                                   //解答関数実行
     }
     catch(e) {
-      conosole.error(e);
+      console.error(e);
     } 
   }
 
@@ -117,7 +134,7 @@ window.addEventListener('load', () => {                                         
     const selectedAnswers = document.getElementsByTagName('input')                                   //動的生成されたinputノードの取得
     for (let selectedAnswer of selectedAnswers) {                                                    //解答選択肢1~4押下時処理
       selectedAnswer.addEventListener('click', () => {
-
+        
         category.remove();                                                                           //カテゴリ消去
         difficulty.remove();                                                                         //難易度消去
         btnA.remove();                                                                               //解答選択肢-1消去
@@ -128,7 +145,7 @@ window.addEventListener('load', () => {                                         
         count++;                                                                                     //取得クイズ数カウントインクリメント
         questionNumber++;                                                                            //クイズNo.インクリメント
 
-        if(count < 10) {                                                                             //クイズ一覧存在時処理
+        if (count < 10) {                                                                             //クイズ一覧存在時処理
           title.textContent = `問題${questionNumber}`;                                                //タイトル列編集
           quizList[count].outDisplay();                                                                //クイズ画面出力メソッド実行
           selectAnswer();                                                                             //解答関数実行
@@ -148,4 +165,13 @@ window.addEventListener('load', () => {                                         
       });
     }
   }
+/*
+  shuffle(arr) {
+    for (let j = arr.length - 1; j > 0; j--) {
+      const k = Math.floor(Math.random() * (j + 1));
+      [arr[k], arr[j] = arr[j], arr[k]];
+    }
+    return arr;
+  }
+*/
 });
